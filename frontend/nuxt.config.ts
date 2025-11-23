88f0106e258191ceec8bf8ba8407a17221f8b560
Stew <ghost.pizza@gmail.com>
2025-11-23 22:40:33 +0100

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'CardShare – Nuxt 3',
+      title: 'ContactShare - Share your profiles with friends',
       meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }]
     }
   },
